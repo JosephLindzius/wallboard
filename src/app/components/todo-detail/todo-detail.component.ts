72a5705bc44fb4cee67b5d8bb685775a5c03d761
Joseph Lindzius <josephlindzius@Josephs-MacBook-Pro.local>
2021-09-27 10:19:29 +0200

    fixed todo component

--- src/app/components/todo-detail/todo-detail.component.ts
+++ src/app/components/todo-detail/todo-detail.component.ts
@@ -1,10 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { Location } from '@angular/common'
-import {TodoService} from "../../services/todo.service";
-import {Observable} from "rxjs";
 import {Todo} from "../../types/types";
-import {ActivatedRoute} from "@angular/router";
-
 
 @Component({
   selector: 'wb-todo-detail',
