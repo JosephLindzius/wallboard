1a8fc69b79a95a180191eba66d9d96bee6268d42
Joseph Lindzius <josephlindzius@Josephs-MacBook-Pro.local>
2021-09-30 23:45:04 +0200

    added env vars

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,20 @@
+const { API_KEY,
+  AUTH_DOMAIN,
+  WALLBOARD_KE,
+  STORAGE_BUCKET,
+  MESSAGING_SENDER_ID,
+  APP_ID,
+  MEASUREMENT_ID} = process.env
+
 export const environment = {
   production: true,
   firebaseConfig: {
-
+    apiKey: API_KEY,
+    authDomain: AUTH_DOMAIN,
+    projectId: WALLBOARD_KE,
+    storageBucket: STORAGE_BUCKET,
+    messagingSenderId: MESSAGING_SENDER_ID,
+    appId: APP_ID,
+    measurementId: MEASUREMENT_ID
   }
 };
